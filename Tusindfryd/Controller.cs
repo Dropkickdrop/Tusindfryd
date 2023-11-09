669f57c43136f0db0d0bdd1578747ae8a92087cf
Martin Peter Molbech <mpmo59239@edu.ucl.dk>
2023-11-09 12:23:23 +0100

    inital commit

---
+++ Tusindfryd/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tusindfryd.Repos;
+
+namespace Tusindfryd
+{
+    internal class Controller
+    {
+        private FlowersortRepo sortRepo;
+        private ProductiontrayRepo trayRepo;
+
+
+    }
+}
---
+++ Tusindfryd/Repos/ProductiontrayRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tusindfryd.Repos
+{
+    internal class ProductiontrayRepo
+    {
+        private List<Productiontray> productiontrays = new List<Productiontray>();
+    }
+}
